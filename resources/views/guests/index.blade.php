17d9f8f58d754242f8f0b79166f056611777ee09
arifpatanduk <arifpatanduk2@gmail.com>
2021-01-17 22:21:01 +0700

    database name = laravel_BukuTamu;

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -1,52 +1,79 @@
-@extends('layout.main')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('title', 'Daftar Tamu UPT TIK UNS')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-@section('container')
-<div class="container">
-    <div class="row">
-        <div class="col-10">
-            <h1 class="mt-3">Daftar Tamu UPT TIK UNS</h1>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
-            <a href="/guests/create" class="btn btn-primary my-3">Tambah Data Tamu</a>
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Tanggal</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Unit</th>
-                        <th scope="col">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($guests as $guest)
-                    <tr>
-                        <th scope="row">{{$loop->iteration}}</th>
-                        <td>{{$guest->created_at}}</td>
-                        <td>{{$guest->nama}}</td>
-                        <td>{{$guest->unit}}</td>
-                        <td>
-                            <a href="/guests/{{$guest->id}}" class="btn btn-primary">Details</a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    <title>Daftar Tamu UPT TIK UNS)</title>
+  </head>
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="{{url('/')}}">Buku Tamu</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="{{url('/guests')}}">Daftar tamu</a>
+          </div>
+        </div>
+      </nav>
+    </div>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-3">Daftar Tamu UPT TIK UNS</h1>
+
+                <a href="/guests/create" class="btn btn-primary my-3">Tambah Data Tamu</a>
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <table class="table display" id="guest" style="width:100%">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Tanggal</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Unit</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($guests as $guest)
+                        <tr>
+                            <th scope="row">{{$loop->iteration}}</th>
+                            <td>{{$guest->created_at}}</td>
+                            <td>{{$guest->nama}}</td>
+                            <td>{{$guest->unit}}</td>
+                            <td>
+                                <a href="/guests/{{$guest->id}}" class="btn btn-primary">Details</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+            </div>
         </div>
     </div>
-</div>
 
 <script>
     $(document).ready(function() {
-    $('#example').DataTable();
-    } );
+    $('#guest').DataTable();
+} );
 </script>
 
-@endsection
+</body>
+</html>
