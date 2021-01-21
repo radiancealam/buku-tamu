f69f02a7ae6821e1e28a9f09fda58f21bb8228ce
arifpatanduk <arifpatanduk2@gmail.com>
2021-01-21 08:27:32 +0700

    Tambahin MODAL detail & input

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -8,6 +8,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
@@ -15,6 +18,7 @@
     <title>Daftar Tamu UPT TIK UNS)</title>
   </head>
   <body>
+    
     <div class="container">
       <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="{{url('/')}}">Buku Tamu</a>
@@ -34,7 +38,63 @@
             <div class="col-10">
                 <h1 class="mt-3">Daftar Tamu UPT TIK UNS</h1>
 
-                <a href="/guests/create" class="btn btn-primary my-3">Tambah Data Tamu</a>
+                <a class="btn btn-primary my-3" data-toggle="modal" data-target="#modal-tambah">Tambah Data Tamu</a>
+
+
+                {{-- MODAL TAMBAH DATA --}}
+                <div class="modal fade" id="modal-tambah" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="tambahModalLabel">Form Tambah Tamu</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <form method="post" action="/guests">
+                        {{-- <form method="post" action="{{ action([GuestsController::class, 'store']) }}"> --}}
+                          @csrf
+
+                          <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Masukkan nama" name="nama" value="{{old('nama')}}">
+                            @error('nama')
+                              <div class="invalid-feedback">
+                                {{ $message }}
+                              </div>
+                            @enderror
+                          </div>
+                          <div class="form-group">
+                            <label for="unit">Unit/Instansi</label>
+                            <input type="text" class="form-control @error('unit') is-invalid @enderror" id="unit" placeholder="Masukkan unit" name="unit" value="{{old('unit')}}">
+                            @error('unit')
+                              <div class="invalid-feedback">
+                                {{ $message }}
+                              </div>
+                            @enderror
+                          </div>
+                          <div class="form-group">
+                            <label for="description">Keterangan</label>
+                            <input type="text" class="form-control @error('description') is-invalid @enderror" id="description" placeholder="Keterangan kegiatan" name="description" value="{{old('description')}}">
+                            @error('description')
+                              <div class="invalid-feedback">
+                                {{ $message }}
+                              </div>
+                            @enderror
+                          </div>
+                          <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Tambah Data</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {{-- END MODAL TAMBAH DATA --}}
+
+
                 @if (session('status'))
                     <div class="alert alert-success">
                         {{ session('status') }}
@@ -58,21 +118,74 @@
                             <td>{{$guest->nama}}</td>
                             <td>{{$guest->unit}}</td>
                             <td>
-                                <a href="/guests/{{$guest->id}}" class="btn btn-primary">Details</a>
+                                <a id="detail" class="btn btn-primary" data-toggle="modal" data-target="#modal-detail"
+                                data-nama = "{{$guest->nama}}"
+                                data-unit = "{{$guest->unit}}"
+                                data-tanggal = "{{$guest->created_at}}"
+                                data-description = "{{$guest->description}}"
+                                >Details</a>
+
+                                <a href="" class="btn btn-success">Edit</a>
+                                <a href="" class="btn btn-danger">Hapus</a>
                             </td>
                         </tr>
+
+                        {{-- MODAL DETAIL --}}
+                        <div class="modal fade" id="modal-detail" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Detail Buku Tamu</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                <h5 id="detail-nama"></h5>
+                                <h6 id="detail-unit" class="mb-2 text-muted"></h6>
+                                <p id="detail-tanggal"></p>
+                                <p id="detail-description"></p>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        {{-- END MODAL DETAIL --}}
+
                         @endforeach
                     </tbody>
                 </table>
-
+              </div>
             </div>
-        </div>
-    </div>
 
+          </div>
+
+{{-- DATATABLE --}}
 <script>
     $(document).ready(function() {
     $('#guest').DataTable();
 } );
+</script>
+
+
+{{-- MODAL DETAIL --}}
+<script>
+  $(document).ready(function(){
+    $(document).on('click', '#detail', function(){
+      var nama = $(this).data('nama');
+      var unit = $(this).data('unit');
+      var tanggal = $(this).data('tanggal');
+      var description = $(this).data('description');
+
+      $('#detail-nama').text(nama);
+      $('#detail-unit').text(unit);
+      $('#detail-tanggal').text(tanggal);
+      $('#detail-description').text(description);
+
+    })
+  });
 </script>
 
 </body>
